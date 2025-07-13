67b4ff534fb9108e1db7677f9912a2c5054734ce
ramnaresh-ahi <ramnareshahi77@gmail.com>
2025-07-13 16:49:01 +0530

    Documentations are going to public

--- macros/tests/assert_positive_value.sql
+++
@@ -1,6 +0,0 @@
-
-{% test assert_positive_value(model, column_name, min_value=0) %}
-  SELECT *
-  FROM {{ model }}
-  WHERE {{ column_name }} < {{ min_value }} OR {{ column_name }} IS NULL
-{% endtest %}
